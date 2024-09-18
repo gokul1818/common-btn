5770b8ab9317dada1c52d14e7fb6dbeb6f29d088
gokul18 <gokulakrishnan.doodleblue@gmail.com>
2024-09-18 17:47:12 +0530

    udpate

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-// src/CommonBtn.js
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
-import PropTypes from 'prop-types';
+// // src/CommonBtn.js
+// import React from 'react';
+// import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+// import PropTypes from 'prop-types';
 
-const CommonBtn = ({ label, onPress }) => {
-  return (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Text style={styles.text}>{label}</Text>
-    </TouchableOpacity>
-  );
+// const CommonBtn = ({ label, onPress }) => {
+//   return (
+//     <TouchableOpacity onPress={onPress} style={styles.button}>
+//       <Text style={styles.text}>{label}</Text>
+//     </TouchableOpacity>
+//   );
+// };
+
+// CommonBtn.propTypes = {
+//   label: PropTypes.string.isRequired,
+//   onPress: PropTypes.func.isRequired,
+// };
+
+// const styles = StyleSheet.create({
+//   button: {
+//     padding: 10,
+//     backgroundColor: 'green',
+//     borderRadius: 5,
+//     alignItems: 'center',
+//   },
+//   text: {
+//     color: '#fff',
+//     fontSize: 16,
+//   },
+// });
+
+// export default CommonBtn;
+
+// src/index.js
+
+const CommonBtn = () => {
+  console.log("Hello from my React Native library!");
 };
 
-CommonBtn.propTypes = {
-  label: PropTypes.string.isRequired,
-  onPress: PropTypes.func.isRequired,
-};
+export { CommonBtn };
 
-const styles = StyleSheet.create({
-  button: {
-    padding: 10,
-    backgroundColor: 'green',
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 16,
-  },
-});
-
-export default CommonBtn;
